0e1d8e879524a4ce19358ad22de4400a08981c08
tesselode <aminnich3@gmail.com>
2015-08-09 23:13:49 -0400

    Just testing some stuff

--- main.lua
+++ main.lua
@@ -6,26 +6,34 @@ function love.load()
     'quit', function(arbitrary)
       return arbitrary + 2
     end,
+    'quit', function(arbitrary)
+      return arbitrary + 3
+    end,
+    'quit', function(arbitrary)
+      return arbitrary + 4
+    end,
     'stupid', function()
       love.graphics.setBackgroundColor(255, 255, 255, 255)
     end
   )
 
-  conversation:stopListening(group)
+  --conversation:stopListening(group)
 end
 
 function love.keypressed(key)
   local arbitrary
 
   if key == 'escape' then
-    arbitrary = conversation:say('quit', 3)
+    arbitrary = {conversation:say('quit', 3)}
+    for k, v in pairs(arbitrary) do
+      print(k, v)
+    end
   elseif key == 'return' then
-    arbitrary = conversation:say('quit', 5)
+    arbitrary = {conversation:say('quit', 5)}
+    for k, v in pairs(arbitrary) do
+      print(k, v)
+    end
   elseif key == ' ' then
     conversation:say('stupid')
-  end
-
-  if arbitrary == 7 then
-    love.event.quit()
   end
 end
