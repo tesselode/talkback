e091e9992a572a9f778d96682b54dac3eab9fa50
Andrew Minnich <aminnich3@gmail.com>
2017-09-29 14:21:21 -0400

    more testing, things seem to be working?

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local talkback = require 'talkback'
 
+-- listener adding and leaving
 do
 	local testVariable = 1
 	local testListener = talkback.listen('hi', function()
@@ -7,13 +8,14 @@ do
 	end)
 	talkback.say 'hi'
 
-	assert(testVariable == 2)
+	assert(testVariable == 2, "test failed: listeners don't respond to group:say()")
 
 	testListener:leave()
 	talkback.say 'hi'
-	assert(testVariable == 2)
+	assert(testVariable == 2, "test failed: listeners don't leave")
 end
 
+-- ignore and reset
 do
 	local test1 = 1
 	local test2 = 1
@@ -24,23 +26,88 @@ do
 	talkback.say 'hi'
 	talkback.say 'hello'
 
-	assert(test1 == 2)
-	assert(test2 == 2)
-	assert(test3 == 2)
+	assert(test1 == 2, "test failed: listeners don't respond to group:say()")
+	assert(test2 == 2, "test failed: listeners don't respond to group:say()")
+	assert(test3 == 2, "test failed: listeners don't respond to group:say()")
 
 	talkback.ignore 'hi'
 	talkback.say 'hi'
 	talkback.say 'hello'
 
-	assert(test1 == 2)
-	assert(test2 == 2)
-	assert(test3 == 3)
+	assert(test1 == 2, "test failed: group:ignore() doesn't work")
+	assert(test2 == 2, "test failed: group:ignore() doesn't work")
+	assert(test3 == 3, "test failed: group:ignore() removes listeners it shouldn't remove")
 
 	talkback.reset()
 	talkback.say 'hi'
 	talkback.say 'hello'
 
-	assert(test1 == 2)
-	assert(test2 == 2)
-	assert(test3 == 3)
+	assert(test1 == 2, "test failed: group:reset() doesn't work")
+	assert(test2 == 2, "test failed: group:reset() doesn't work")
+	assert(test3 == 3, "test failed: group:reset() doesn't work")
+end
+
+-- groups
+do
+	local group = talkback.newGroup()
+	local subGroup = group:newGroup()
+	local test = 1
+	talkback.listen('hi', function() test = test + 1 end)
+	group:listen('hi', function() test = test + 1 end)
+	subGroup:listen('hi', function() test = test + 1 end)
+
+	talkback.say 'hi'
+	assert(test == 4, "test failed: group:say() doesn't call nested groups correctly")
+	group:say 'hi'
+	assert(test == 6, "test failed: group:say() doesn't call nested groups correctly")
+	subGroup:say 'hi'
+	assert(test == 7, "test failed: group:say() doesn't call nested groups correctly")
+
+	group:leave()
+	talkback.say 'hi'
+	assert(test == 8, "test failed: group:leave() doesn't work")
+
+	group = talkback.newGroup()
+	group:listen('hi', function() test = test + 1 end)
+	talkback.say 'hi'
+	assert(test == 10, "test failed: listeners don't respond to group:say()")
+	talkback.reset()
+	talkback.say 'hi'
+	assert(test == 10, "test failed: talkback.reset() doesn't remove groups")
+end
+
+-- returns
+do
+	local group = talkback.newGroup()
+	local subGroup = group:newGroup()
+	subGroup:listen('hi', function()
+		return 'somebody', 'once'
+	end)
+	group:listen('hi', function()
+		return 'told', 'me', 'the'
+	end)
+	talkback.listen('hi', function()
+		return 'world', 'is', 'gonna', 'roll', 'me'
+	end)
+
+	local responses = {talkback.say 'hi'}
+	assert(#responses == 10, "talkback.say doesn't return responses correctly")
+	assert(responses[1] == 'somebody', "talkback.say doesn't return responses correctly")
+	assert(responses[2] == 'once', "talkback.say doesn't return responses correctly")
+	assert(responses[3] == 'told', "talkback.say doesn't return responses correctly")
+	assert(responses[4] == 'me', "talkback.say doesn't return responses correctly")
+	assert(responses[5] == 'the', "talkback.say doesn't return responses correctly")
+	assert(responses[6] == 'world', "talkback.say doesn't return responses correctly")
+	assert(responses[7] == 'is', "talkback.say doesn't return responses correctly")
+	assert(responses[8] == 'gonna', "talkback.say doesn't return responses correctly")
+	assert(responses[9] == 'roll', "talkback.say doesn't return responses correctly")
+	assert(responses[10] == 'me', "talkback.say doesn't return responses correctly")
+
+	local responses = {group:say 'hi'}
+	assert(#responses == 5, "group:say() is returning responses from parent groups, somehow")
+	assert(responses[1] == 'somebody', "group:say() is returning responses from parent groups, somehow")
+	assert(responses[2] == 'once', "group:say() is returning responses from parent groups, somehow")
+	assert(responses[3] == 'told', "group:say() is returning responses from parent groups, somehow")
+	assert(responses[4] == 'me', "group:say() is returning responses from parent groups, somehow")
+	assert(responses[5] == 'the', "group:say() is returning responses from parent groups, somehow")
 end
--- talkback.lua
+++ talkback.lua
@@ -25,15 +25,21 @@ end
 
 function Group:say(message, ...)
 	local allResponses = {}
+	for i = 1, #self._groups do
+		local responses = {self._groups[i]:say(message, ...)}
+		for j = 1, #responses do
+			table.insert(allResponses, responses[j])
+		end
+	end
 	for i = 1, #self._listeners do
-		if #self._listeners[i]._message == message then
+		if self._listeners[i]._message == message then
 			local responses = {self._listeners[i]._f(...)}
 			for j = 1, #responses do
 				table.insert(allResponses, responses[j])
 			end
 		end
 	end
-	return allResponses
+	return unpack(allResponses)
 end
 
 function Group:leave()
@@ -47,30 +53,34 @@ end
 
 function Group:ignore(message)
 	for i = #self._listeners, 1, -1 do
-		if self._listeners[i].message == message then
+		if self._listeners[i]._message == message then
 			table.remove(self._listeners, i)
 		end
 	end
 end
 
 function Group:reset()
+	for i = #self._groups, 1, -1 do
+		table.remove(self._groups, i)
+	end
 	for i = #self._listeners, 1, -1 do
 		table.remove(self._listeners, i)
 	end
 end
 
 function Group:newGroup()
-	return setmetatable({
+	local group = setmetatable({
 		_listeners = {},
 		_groups = {},
 		_parent = self,
 	}, {__index = Group})
+	if self._groups then
+		table.insert(self._groups, group)
+	end
+	return group
 end
 
 local baseGroup = Group:newGroup()
-
-talkback.baseGroup = baseGroup
-
 function talkback.listen(...) return baseGroup:listen(...) end
 function talkback.say(...) return baseGroup:say(...) end
 function talkback.ignore(...) return baseGroup:ignore(...) end
